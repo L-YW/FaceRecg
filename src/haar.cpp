dd0fcf156f664b1d5946c565f5b8a1fc44a8c21d
=L-YW <=>
2020-10-05 16:50:45 +0900

    serial ok

--- src/haar.cpp
+++ src/haar.cpp
@@ -3,24 +3,32 @@
 #include "opencv2/highgui.hpp"
 #include "opencv2/imgproc.hpp"
 
+#include <wiringSerial.h>
+#include <wiringPi.h>
+
+#include <string.h>
 #include <stdio.h>
- 
+
+#define BAUD    115200
 using namespace std;
 using namespace cv;
- 
-void detect_face(){
-    Mat src = imread("../data/face_y.jpg");
 
+char serc[15] = "0314228071";
+
+char detect_face(){
+    int ser;
+    
+    Mat src = imread("../data/face_y.jpg");
     if(src.empty()){
         printf("Image load failed!\n");
-        return;
+        return 1;
     }
     
     CascadeClassifier classifier("./haarcascade_frontalface_default.xml");
 
     if(classifier.empty()){
         printf("XML load failed!\n");
-        return;
+        return 1;
     }
 
     vector<Rect> faces;
@@ -29,19 +37,26 @@ void detect_face(){
     for(Rect rc : faces){
         rectangle(src, rc, Scalar(255, 0, 255), 2);
     }
+    
     if(faces.size() == 0){
-        printf("0314228071\n");
+        serc[5] = '2';
     }
     else
     {
-        printf("0314208071\n");
+        serc[5] = '0';
     }
-    
-    // imshow("src", src);
-    // waitKey();
-    // destroyAllWindows();
 }
-
 int main(){
+    int fd;
     detect_face();
+//    serc[5] = (char)(detect_face()+48);
+    if((fd=serialOpen("/dev/ttyAMA0", BAUD)) < 0){
+        printf("Device file open error!! use sudo ...\n");
+        return 1;
+    }    
+    serialPuts(fd, serc);
+    serialPutchar(fd, '\r');
+    serialPutchar(fd, '\n');
+
+    return 0;
 }
